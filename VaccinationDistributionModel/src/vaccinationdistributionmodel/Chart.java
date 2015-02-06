9f93246995c85a12c2fb003d2f9959bc3db99890
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-02-06 23:04:17 +0200

    refactored chart drawing

--- VaccinationDistributionModel/src/vaccinationdistributionmodel/Chart.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/Chart.java
@@ -1,7 +1,10 @@
 package vaccinationdistributionmodel;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -9,6 +12,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -16,78 +20,28 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class Chart extends JFrame {
 	private History history;
 	private XYSeriesCollection dataset = new XYSeriesCollection();
-	
+	private XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        
 	public Chart(History history) {
 		super("Chart");
 		this.history = history;
 	}
 	
 	public void draw() {
-		/*this.population = population;
-		this.susceptible = susceptible;
-		this.exposed = exposed;
-		this.infected = infected;
-		this.advanced = advanced;
-		this.recovered = recovered;
-		this.dead = dead;*/
 		
-		XYSeries population = new XYSeries("Population");
-		List<CityState> states = this.history.getStates();
-		
-		for (int i = 1; i < states.size(); i++) {
-			population.add(i, states.get(i - 1).population);
-		}
-		
-		this.dataset.addSeries(population);
-		
-		XYSeries susceptible = new XYSeries("Susceptible");
-		
-		for (int i = 1; i < states.size(); i++) {
-			susceptible.add(i, states.get(i - 1).susceptible);
-		}
-		
-		this.dataset.addSeries(susceptible);
-		
-		XYSeries exposed = new XYSeries("Exposed");
-		
-		for (int i = 1; i < states.size(); i++) {
-			exposed.add(i, states.get(i - 1).exposed);
-		}
-		
-		this.dataset.addSeries(exposed);
-		
-		XYSeries infected = new XYSeries("Infected");
-		
-		for (int i = 1; i < states.size(); i++) {
-			infected.add(i, states.get(i - 1).infected);
-		}
-		
-		this.dataset.addSeries(infected);
-		
-		XYSeries advanced = new XYSeries("Advanced");
-		
-		for (int i = 1; i < states.size(); i++) {
-			advanced.add(i, states.get(i - 1).advanced);
-		}
-		
-		this.dataset.addSeries(advanced);
-		
-		XYSeries recovered = new XYSeries("Recovered");
-		
-		for (int i = 1; i < states.size(); i++) {
-			recovered.add(i, states.get(i - 1).recovered);
-		}
-		
-		this.dataset.addSeries(recovered);
-		
-		XYSeries dead = new XYSeries("Dead");
-		
-		for (int i = 1; i < states.size(); i++) {
-			dead.add(i, states.get(i - 1).dead);
-		}
-		
-		this.dataset.addSeries(dead);
-		
+                Map<String, List<Integer>> lines = this.history.getData();
+                
+                int id = 0;
+                for (Entry<String, List<Integer>> e: lines.entrySet()){
+                    XYSeries series = new XYSeries(e.getKey());
+                    List<Integer> values = e.getValue();
+                    for (int i=1; i < values.size(); i++){
+                        series.add(i, values.get(i-1));
+                    }
+                    this.renderer.setSeriesStroke(id++, new BasicStroke(2.0f));
+                    this.dataset.addSeries(series);
+                }
+                		
 		JFreeChart chart = createChart(this.dataset);
 		ChartPanel chartPanel = new ChartPanel(chart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
@@ -116,11 +70,8 @@ public class Chart extends JFrame {
 		plot.setDomainGridlinePaint(Color.white);
 		plot.setRangeGridlinePaint(Color.white);
 		// plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
-
-		/*XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-		renderer.setSeriesLinesVisible(0, false);
-		renderer.setSeriesShapesVisible(1, false);
-		plot.setRenderer(renderer);*/
+                
+		plot.setRenderer(this.renderer);
 
 		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
 		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
--- VaccinationDistributionModel/src/vaccinationdistributionmodel/History.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/History.java
@@ -1,7 +1,9 @@
 package vaccinationdistributionmodel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class History {
 	private List<CityState> states;
@@ -13,6 +15,30 @@ public class History {
 	public List<CityState> getStates() {
 		return this.states;
 	}
+        
+        public Map<String, List<Integer>> getData(){
+            HashMap<String, List<Integer>> map = new HashMap<>();
+            
+            map.put("population", new ArrayList<>());
+            map.put("susceptible", new ArrayList<>());
+            map.put("exposed", new ArrayList<>());
+            map.put("infected", new ArrayList<>());
+            map.put("advanced", new ArrayList<>());
+            map.put("recovered", new ArrayList<>());
+            map.put("dead", new ArrayList<>());
+            
+            for (CityState state: this.states){
+                map.get("population").add(state.population);
+                map.get("susceptible").add(state.susceptible);
+                map.get("exposed").add(state.exposed);
+                map.get("infected").add(state.infected);
+                map.get("advanced").add(state.advanced);
+                map.get("recovered").add(state.recovered);
+                map.get("dead").add(state.dead);
+            }
+            
+            return map;
+        }
 	
 	public void addState(CityState state) {
 		this.states.add(state);
