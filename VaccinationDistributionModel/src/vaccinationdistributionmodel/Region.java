aa2f867b018b5cb84053a0868e9926f7f8df15ca
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-02-06 20:24:35 +0200

    further implemented region parameters

--- VaccinationDistributionModel/src/vaccinationdistributionmodel/Region.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/Region.java
@@ -6,10 +6,43 @@
 
 package vaccinationdistributionmodel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ilari
  */
-public class Region {
+public class Region implements Modelable{
     
+    ArrayList<Region> neighbouringRegions;
+    ArrayList<City> cities;
+    double mortality; // rate of death from ebola
+    double infectiveness; // rate of spread of ebola
+    int [][] weights;
+    
+    public Region(double mortality, double infectiveness) {
+        this(mortality, infectiveness, new ArrayList<>());
+    }
+    
+    public Region(double mortality, double infectiveness, ArrayList<City> cities) {
+        this.cities = cities;
+        this.mortality = mortality;
+        this.infectiveness = infectiveness;
+        
+        init();
+    }
+    
+    private void init(){
+        for (City c: cities){
+            c.setParameters(mortality, infectiveness);
+        }
+    }
+    
+    
+    @Override
+    public void update() {
+        for (City city: cities){
+            city.update();
+        }
+    }
 }
