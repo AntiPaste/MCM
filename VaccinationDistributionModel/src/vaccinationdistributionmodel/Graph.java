f235b8b934ef6b65557b1da165650d8613264eb9
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-02-07 13:41:42 +0200

    Graph almost done, added getNodes

--- VaccinationDistributionModel/src/vaccinationdistributionmodel/Graph.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/Graph.java
@@ -5,7 +5,9 @@
  */
 package vaccinationdistributionmodel;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -14,14 +16,40 @@ import java.util.List;
  */
 public class Graph<T> {
 
-    private List<Edge<T>> adjancencyList;
+    private List<Edge<T>> edges;
+    private List<T> nodes;
+    private Map<T, List<Edge<T>>> neighbours;
 
-    public Graph(List<Edge<T>> neighbours) {
-        this.adjancencyList = neighbours;
+    public Graph(List<Edge<T>> edges) {
+        this.edges = edges;
+        this.generateNodesList();
+    }
+    
+    private void generateNodesList(){
+        this.nodes = new ArrayList<>();
+        for (Edge<T> edge : this.edges){
+            if (!nodes.contains(edge.one)) nodes.add(edge.one);
+            if (!nodes.contains(edge.other)) nodes.add(edge.other);
+        }
+    }
+    
+    public List<T> getNodes(){
+        return this.nodes;
+    }
+    
+    private void initMap(){
+        
+    }
+    
+    public List<Edge<T>> getEdges(T node){
+        if (this.neighbours == null){
+            this.initMap();
+        }
+        return this.neighbours.get(node);
     }
 
     public List<Edge<T>> edges() {
-        return this.adjancencyList;
+        return this.edges;
     }
 
 }
