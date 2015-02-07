b2df6c5bd034d74bd71236e42356dfb09e655b5a
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-02-07 12:20:13 +0200

    vaccination method for delivering n shots to a city

--- VaccinationDistributionModel/src/vaccinationdistributionmodel/City.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/City.java
@@ -2,78 +2,78 @@ package vaccinationdistributionmodel;
 
 public class City implements Modelable {
 
-	private History history;
-	private CityState values;
-	private Parameters parameters;
+    private History history;
+    private CityState values;
+    private Parameters parameters;
 
-	public City(int population) {
-		this(population, 0, 0, 0, 0, 0, 0);
-	}
+    public City(int population) {
+        this(population, 0, 0, 0, 0, 0, 0);
+    }
 
-	public City(int population, int susceptible, int exposed,
-			int infected, int advanced, int recovered, int dead) {
-		this.values = new CityState(population, susceptible, exposed,
-				infected, advanced, recovered, dead);
+    public City(int population, int susceptible, int exposed,
+            int infected, int advanced, int recovered, int dead) {
+        this.values = new CityState(population, susceptible, exposed,
+                infected, advanced, recovered, dead);
 
-		this.history = new History();
-		this.history.addState(this.values);
-	}
+        this.history = new History();
+        this.history.addState(this.values);
+    }
 
-	public History getHistory() {
-		return this.history;
-	}
+    public History getHistory() {
+        return this.history;
+    }
 
-	public void setParameters(Parameters parameters) {
-		this.parameters = parameters;
-	}
+    public void setParameters(Parameters parameters) {
+        this.parameters = parameters;
+    }
 
-	public void interact(City other, double weight) {
-		
-	}
+    public void interact(City other, double weight) {
 
-	@Override
-	public void update() {
-		// infected -> recovered
-		int peopleToRecover = (int) (this.values.infected * this.parameters.recoveryRate);
+    }
 
-		this.values.infected -= peopleToRecover;
-		this.values.recovered += peopleToRecover;
+    @Override
+    public void update() {
+        // infected -> recovered
+        int peopleToRecover = (int) (this.values.infected * this.parameters.recoveryRate);
 
-		// infected -> removed
-		int peopleToKill = (int) (this.values.infected * this.parameters.mortalityRate);
+        this.values.infected -= peopleToRecover;
+        this.values.recovered += peopleToRecover;
 
-		this.values.infected -= peopleToKill;
-		this.values.dead += peopleToKill;
+        // infected -> removed
+        int peopleToKill = (int) (this.values.infected * this.parameters.mortalityRate);
 
-		// exposed -> infected
-		int peopleToInfect = (int) (this.values.exposed * this.parameters.infectionRate);
+        this.values.infected -= peopleToKill;
+        this.values.dead += peopleToKill;
 
-		this.values.exposed -= peopleToInfect;
-		this.values.infected += peopleToInfect;
+        // exposed -> infected
+        int peopleToInfect = (int) (this.values.exposed * this.parameters.infectionRate);
 
-		// susceptible -> exposed
-		int peopleToContaminate = (int) ((this.values.susceptible * this.values.infected * this.parameters.contaminationRate)
-				/ (this.values.susceptible + this.values.infected));
+        this.values.exposed -= peopleToInfect;
+        this.values.infected += peopleToInfect;
 
-		this.values.susceptible -= peopleToContaminate;
-		this.values.exposed += peopleToContaminate;
+        // susceptible -> exposed
+        int peopleToContaminate = (int) ((this.values.susceptible * this.values.infected * this.parameters.contaminationRate)
+                / (this.values.susceptible + this.values.infected));
 
-		this.history.addState(this.values);
-	}
+        this.values.susceptible -= peopleToContaminate;
+        this.values.exposed += peopleToContaminate;
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		double fPopulation = (double) this.values.population;
+        this.history.addState(this.values);
+    }
 
-		builder.append(String.format("Population: %d\n", this.values.population));
-		builder.append(String.format("Susceptible: %d (%.2f%%)\n", this.values.susceptible, this.values.susceptible / fPopulation * 100));
-		builder.append(String.format("Exposed: %d (%.2f%%)\n", this.values.exposed, this.values.exposed / fPopulation * 100));
-		builder.append(String.format("Infected: %d (%.2f%%)\n", this.values.infected, this.values.infected / fPopulation * 100));
-		builder.append(String.format("Advanced: %d (%.2f%%)\n", this.values.advanced, this.values.advanced / fPopulation * 100));
-		builder.append(String.format("Recovered: %d (%.2f%%)\n", this.values.recovered, this.values.recovered / fPopulation * 100));
-		builder.append(String.format("Dead: %d (%.2f%%)\n", this.values.dead, this.values.dead / fPopulation * 100));
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        double fPopulation = (double) this.values.population;
 
-		return builder.toString();
-	}
+        builder.append(String.format("Population: %d\n", this.values.population));
+        builder.append(String.format("Susceptible: %d (%.2f%%)\n", this.values.susceptible, this.values.susceptible / fPopulation * 100));
+        builder.append(String.format("Exposed: %d (%.2f%%)\n", this.values.exposed, this.values.exposed / fPopulation * 100));
+        builder.append(String.format("Infected: %d (%.2f%%)\n", this.values.infected, this.values.infected / fPopulation * 100));
+        builder.append(String.format("Advanced: %d (%.2f%%)\n", this.values.advanced, this.values.advanced / fPopulation * 100));
+        builder.append(String.format("Recovered: %d (%.2f%%)\n", this.values.recovered, this.values.recovered / fPopulation * 100));
+        builder.append(String.format("Dead: %d (%.2f%%)\n", this.values.dead, this.values.dead / fPopulation * 100));
+
+        return builder.toString();
+    }
 }
