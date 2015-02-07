73281723c42c81eadbd6099da2af68f7db2f5708
Kasper Koho <antipaste@gmail.com>
2015-02-07 12:48:03 +0200

    AreaChart

--- VaccinationDistributionModel/src/vaccinationdistributionmodel/AreaChart.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/AreaChart.java
@@ -1,48 +1,53 @@
 package vaccinationdistributionmodel;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
 
-public class Chart extends JFrame {
+public class AreaChart extends JFrame {
 
 	private History history;
-	private XYSeriesCollection dataset = new XYSeriesCollection();
-	private XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+	private CategoryDataset dataset;
 
-	public Chart(History history) {
+	public AreaChart(History history) {
 		super("Chart");
 		this.history = history;
 	}
 
 	public void draw() {
 		Map<String, List<Integer>> lines = this.history.getData();
+		double[][] data = new double[lines.size() - 1][];
 
 		int id = 0;
 		for (Map.Entry<String, List<Integer>> e : lines.entrySet()) {
-			XYSeries series = new XYSeries(e.getKey());
-			List<Integer> values = e.getValue();
-			
-			for (int i = 1; i < values.size(); i++) {
-				series.add(i, values.get(i - 1));
+			if (e.getKey().equals("population")) {
+				continue;
 			}
-			
-			this.renderer.setSeriesStroke(id++, new BasicStroke(2.0f));
-			this.dataset.addSeries(series);
+
+			List<Integer> values = e.getValue();
+
+			double[] entries = new double[values.size()];
+
+			for (int i = 0; i < values.size(); i++) {
+				entries[i] = values.get(i);
+			}
+
+			data[id++] = entries;
 		}
+
+		this.dataset = DatasetUtilities.createCategoryDataset(
+				"Series ", "Type ", data
+		);
 
 		JFreeChart chart = createChart(this.dataset);
 		ChartPanel chartPanel = new ChartPanel(chart);
@@ -50,8 +55,8 @@ public class Chart extends JFrame {
 		setContentPane(chartPanel);
 	}
 
-	private JFreeChart createChart(final XYDataset dataset) {
-		JFreeChart chart = ChartFactory.createXYLineChart(
+	private JFreeChart createChart(CategoryDataset dataset) {
+		JFreeChart chart = ChartFactory.createStackedAreaChart(
 				null, // chart title
 				"Days", // x axis label
 				"Values", // y axis label
@@ -64,13 +69,17 @@ public class Chart extends JFrame {
 
 		chart.setBackgroundPaint(Color.white);
 
-		XYPlot plot = chart.getXYPlot();
+		CategoryPlot plot = (CategoryPlot) chart.getPlot();
+		plot.setForegroundAlpha(0.5f);
 		plot.setBackgroundPaint(Color.lightGray);
 		plot.setDomainGridlinePaint(Color.white);
 		plot.setRangeGridlinePaint(Color.white);
 
-		plot.setRenderer(this.renderer);
+		CategoryAxis domainAxis = plot.getDomainAxis();
+		domainAxis.setLowerMargin(0.0);
+		domainAxis.setUpperMargin(0.0);
 
+		// change the auto tick unit selection to integer units only...
 		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
 		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
--- VaccinationDistributionModel/src/vaccinationdistributionmodel/VaccinationDistributionModel.java
+++ VaccinationDistributionModel/src/vaccinationdistributionmodel/VaccinationDistributionModel.java
@@ -11,37 +11,36 @@ package vaccinationdistributionmodel;
  */
 public class VaccinationDistributionModel {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("<b>Ebola</b>");
-
-        Parameters parameters = new Parameters();
-        parameters.contaminationRate = 1;
-        parameters.infectionRate = 0.1;
-        parameters.mortalityRate = 0.1;
-        parameters.recoveryRate = 0.1;
-
-        City city = new City(10000, 9900, 100, 0, 0, 0, 0);
-        city.setParameters(parameters);
-
-        System.out.println(city);
-
-        for (int i = 0; i < 100; i++) {
-            city.update();
-            System.out.println(city);
-
-            try {
-                //Thread.sleep(3000);
-            } catch (Exception e) {
-            }
-        }
-
-        Chart chart = new Chart(city.getHistory());
-        chart.draw();
-        chart.pack();
-        chart.setVisible(true);
-    }
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("<b>Ebola</b>");
+		
+		Parameters parameters = new Parameters();
+		parameters.contaminationRate = 1;
+		parameters.infectionRate = 0.1;
+		parameters.mortalityRate = 0.1;
+		parameters.recoveryRate = 0.1;
+		
+		City city = new City(10000, 9900, 100, 0, 0, 0, 0);
+		city.setParameters(parameters);
+		
+		System.out.println(city);
+		
+		for (int i = 0; i < 100; i++) {
+			city.update();
+			System.out.println(city);
+			
+			try {
+				//Thread.sleep(3000);
+			} catch (Exception e) {}
+		}
+		
+		AreaChart chart = new AreaChart(city.getHistory());
+		chart.draw();
+		chart.pack();
+		chart.setVisible(true);
+	}
 
 }
